03e4e4f4f3a482d610ebdb7a563f2938a040f132
testing <aldhigandia1@gmail.com>
2023-06-19 17:16:04 +0700

    add: depelov

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const connectDB = require('./src/database');
 
-app.listen(port, () => {
-    console.log(`application running on port ${port}`);
-})
+const app = express();
+
+connectDB();
+
+/**
+ * routing
+ */
+const routesApiV1 = require('./src/routes/v1routes');
+
+app.use('/api/v1', routesApiV1);
+app
+  .route('*')
+  .get((req, res) => {
+    res.send("you're inside fallback route");
+  })
+  .post((req, res) => {
+    res.send("you're inside fallback route");
+  })
+  .put((req, res) => {
+    res.send("you're inside fallback route");
+  })
+  .delete((req, res) => {
+    res.send("you're inside fallback route");
+  });
+
+module.exports = app;
