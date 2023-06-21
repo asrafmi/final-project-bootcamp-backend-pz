9c4c11e5e821fa9a33ec6babe70b4c0185e87a41
asrafmi <asraf.muhammad07@gmail.com>
2023-06-21 20:08:32 +0700

    feat: set up swagger

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const swaggerUI = require('swagger-ui-express');
+const swagger = require('./src/docs/swagger.json')
 const connectDB = require("./src/database/product");
 
 const app = express();
@@ -11,6 +13,8 @@ connectDB();
 const routesApiV1 = require("./src/routes/route-v1");
 
 app.use("/api/v1", routesApiV1);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swagger))
+
 app
   .route("*")
   .get((req, res) => {
@@ -25,6 +29,7 @@ app
   .delete((req, res) => {
     res.send("you're inside fallback route");
   });
+
 
 app.listen(3000, () => {
   console.log("application listen on http://localhost:3000");
