1236fe91e5661348ee3b3ff017f261b7bf1cd305
asrafmi <asraf.muhammad07@gmail.com>
2023-06-24 17:41:27 +0700

    feat: swagger for product

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -1,24 +1,59 @@
+const { to } = require('await-to-js')
 const productSvc = require("../services/product");
 
 async function fetch(req, res) {
-  const data = await productSvc.fetch();
-  res.send(data);
+  try {
+    const data = await productSvc.fetch();
+    res.send(data);
+  } catch (error) {
+    res.status(500)
+  }
 }
 async function getOne(req, res) {
-  const data = await productSvc.getOne(req.params.id);
-  res.send(data);
+  try {
+    const [err, data] = await to(productSvc.getOne(req.params.id))
+    if (err) {
+      throw new Error('Data not found!')
+    }
+    res.send(data);
+  } catch (error) {
+    if (error.message === 'Data not found!') {
+      res.send({
+        status: 404,
+        message: `${error.message}`
+      })
+    } else {
+      res.send({
+        status: 500,
+        message: `${error.message}`
+      })
+    }
+  }
 }
 async function create(req, res) {
-  const product = await productSvc.create(req.body);
-  res.send(product);
+  try {
+    const product = await productSvc.create(req.body);
+    res.send(product);
+  } catch (error) {
+    res.status(500)
+  }
 }
 async function update(req, res) {
-  const data = await productSvc.update(req.body, req.params.id);
-  res.send(data);
+  try {
+    const data = await productSvc.update(req.body, req.params.id);
+    console.log('halooo');
+    res.send(data);
+  } catch (error) {
+    res.status(500)
+  }
 }
 async function destroy(req, res) {
-  const data = await productSvc.destroy(req.params.id);
-  res.send(data);
+  try {
+    const data = await productSvc.destroy(req.params.id);
+    res.send(data);
+  } catch (error) {
+    res.status(500)
+  }
 }
 
 module.exports = {
